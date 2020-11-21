e1e9c7369704a9abf77a960aff814274c03b656a
Agustin Miguel <agustinmiguel96@gmail.com>
2020-11-21 16:27:38 -0300

    test

--- src/test/java/tests/viajeTest2.java
+++ src/test/java/tests/viajeTest2.java
@@ -1,33 +1,42 @@
 package tests;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.BeforeClass;
-import java.util.List;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 import demo.controller.ViajeController;
 import demo.model.Viaje;
 import demo.repository.ViajeRepository;
 
-public class viajeTest2 {
-	static ViajeController viajeController;
-	
-	@BeforeClass
-	public static void beforeClass() {
-		ViajeController viajeController = new ViajeController(ViajeRepository repository);
-	}
+public class viajeTest2{
 
-	@Test
-	public void test() {
-		int count = 0;
-		Iterable<Viaje> caca = viajeController.getViajes();
-		for (Viaje v : caca) {
-			count++;
-		}
-		assertEquals(1, count);
-	}
+
+    private ViajeRepository viajeRepository = Mockito.mock(ViajeRepository.class);
+    private ViajeController viajeController = new ViajeController(viajeRepository);
+
+
+    @BeforeClass
+    public static void beforeClass() {
+        //ViajeRepository repository = null;
+    //    ViajeController viajeController = new ViajeController(repository);
+    }
+
+
+    @Test
+    public void test() {
+        int count = 0;
+        Iterable<Viaje> caca =  viajeController.getViajes();
+        for (Viaje v : caca) {
+            count++;
+        }
+//        System.out.println(caca.getId());
+        System.out.println(count);
+        assertEquals(3, count);
+    }
+
 }
-
