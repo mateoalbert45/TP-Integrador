2009039be3ee33cf8e6c151450365c1ec55c0d16
Agustin Miguel <agustinmiguel96@gmail.com>
2020-11-22 16:53:02 -0300

    test

--- src/test/java/tests/viajeTest2.java
+++ src/test/java/tests/viajeTest2.java
@@ -1,42 +1,48 @@
 package tests;
 
+import static org.hamcrest.CoreMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.junit.Assert.assertEquals;
+import java.time.LocalDate;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import demo.controller.ViajeController;
 import demo.model.Viaje;
 import demo.repository.ViajeRepository;
+import io.swagger.v3.oas.models.media.MediaType;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(ViajeController.class)
 public class viajeTest2{
-
-
-    private ViajeRepository viajeRepository = Mockito.mock(ViajeRepository.class);
-    private ViajeController viajeController = new ViajeController(viajeRepository);
-
-
-    @BeforeClass
-    public static void beforeClass() {
-        //ViajeRepository repository = null;
-    //    ViajeController viajeController = new ViajeController(repository);
-    }
-
-
-    @Test
-    public void test() {
-        int count = 0;
-        Iterable<Viaje> caca =  viajeController.getViajes();
-        for (Viaje v : caca) {
-            count++;
-        }
-//        System.out.println(caca.getId());
-        System.out.println(count);
-        assertEquals(3, count);
-    }
-
+	@MockBean
+	ViajeRepository viajeRepository;
+	
+	ObjectMapper mapper = new ObjectMapper();
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void createViaje() throws Exception {
+	 	LocalDate fecha3 = LocalDate.of(2020, 10, 10);
+	 	LocalDate fecha4 = LocalDate.of(2020, 10, 11);
+		Viaje viaje = new Viaje(Long.valueOf("1"), "viajeA", fecha3, fecha4, "viajeA");
+		when(viajeRepository.save(viaje));
+		((MockHttpServletRequestBuilder) mockMvc.perform(post("http://localhost:8080/viaje/add")))
+			.content(mapper.writeValueAsString(viaje))
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpected(status().isOk()));
+	}
 }
